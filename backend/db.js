eeee96b04866557eea29ebd97a1a9b9b2c0f83be
Harsh Jyoriya <harshjyoriyagdg24@gmail.com>
2025-01-14 13:19:57 +0530

    46- added express validtor in auth.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URL = "mongodb+srv://harshjyoriya:harsh789456123@cluster0.gikk3.mongodb.net";
+// const URL = "mongodb+srv://harshjyoriya:harsh789456123@cluster0.gikk3.mongodb.net";
+const URL = "mongodb+srv://harshjyoriya:harsh789456123@cluster0.gikk3.mongodb.net/notepad?retryWrites=true&w=majority";
+
 
 const connectToMongo = async () => {
   try {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express =require('express');
 const router= express.Router();
-const User=require('../models/User')
+const User=require('../models/User');
+const { check, validationResult } = require('express-validator');
 
 
 //Create a User using: POST "/api/auth/" . Doesn't require Auth
-router.post('/', (req, res)=>{
-    console.log(req.body)
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+     check('name').isLength({min:3}).withMessage('Name must be at least 3 characters long'),
+     check('email').isEmail().withMessage('Please provide a valid email'),
+     check('password').isLength({min:5}).withMessage('Password must be at least 3 characters long'),
+], (req, res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+    // console.log(req.body)
+    // const user=User(req.body);
+    // user.save();
+    // res.send(req.body);
+
+    User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    }).then(user=>res.json(user))
+    .catch(err=>{console.log(err)
+        res.json({error: 'Please a unique email'})
+    })
 })
 
 module.exports=router;
